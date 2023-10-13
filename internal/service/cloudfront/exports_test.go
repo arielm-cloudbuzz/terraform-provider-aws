66b41dbdc4395585cc9d1fc835cc029eaaa3d1a1
Ariel Maloul <arielm@cloudbuzz.co.il>
2023-10-13 21:38:16 +0300

    remove all unecessary services from internal/service

--- internal/service/cloudfront/exports_test.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
-package cloudfront
-
-// Exports for use in tests only.
-var (
-	ResourceContinuousDeploymentPolicy = newResourceContinuousDeploymentPolicy
-)
